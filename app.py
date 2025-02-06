e33c87e1fdc50e9315a4671bbe81b157483e5b08
Vinay1223 <vinaykrishnamandem@gmail.com>
2025-02-06 23:44:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from langchain.prompts import PromptTemplate
 from langchain_groq import ChatGroq
 from langchain.chains.summarize import load_summarize_chain
 from langchain_community.document_loaders import YoutubeLoader,UnstructuredURLLoader
-
+## ##
 
 ##### streamlit APP
 st.set_page_config(page_title="LangChain: Summarize Text From YT or Website", page_icon="🦜")
